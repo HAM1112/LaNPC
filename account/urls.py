c00036cc1bd49c4f45b10186d3d2e1598efff370
Hamdan Salih <hamdansalihpc@gmail.com>
2023-09-11 18:20:22 +0530

    account urls code sturcture

--- account/urls.py
+++ account/urls.py
@@ -3,13 +3,12 @@ from . import views
 
 urlpatterns = [
     path('' , views.signUp , name='acc-signup'),
-    path('verify_otp/' , views.verifyOtp , name='verify-otp'),
     path('signin/' , views.signIn , name='acc-signin'),
     
-    # payment urls
+    path('verify_otp/' , views.verifyOtp , name='verify-otp'),
     
+    # payment urls   
     path('payment/<int:packId>/' , views.payment , name='payment' ),
     path('payment-completed/' , views.payment_completed_view , name='payment-completed' ),
     path('payment-failed/' , views.payment_failed_view , name='payment-failed' ),
-    # path('payment-canceled/' , views.payment_canceled_view , name='payment-canceled' ),
 ]
