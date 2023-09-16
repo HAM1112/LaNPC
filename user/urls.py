f2b8d66312d6310a34213baf7caa1bec1a79ee58
Hamdan Salih <hamdansalihpc@gmail.com>
2023-09-16 11:41:11 +0530

    user url code sturcture

--- user/urls.py
+++ user/urls.py
@@ -5,19 +5,18 @@ urlpatterns = [
     path('home/', views.home , name='home' ),
     path('browse/', views.browse , name='browse' ),
     path('profile/', views.profile , name='profile' ),
-    path('game/<int:gameId>/' , views.gameDetails , name='game'),
-    path('allgames/' , views.allGames , name='all-games'),
-    path('coins/', views.coins , name='coins'),
-    path('category/<int:categoryId>/' , views.categoryGame , name='category-game'),
-    path('logout/', views.userLogout, name='user-logout'),
     
+    path('allgames/' , views.allGames , name='all-games'),
+    path('game/<int:gameId>/' , views.gameDetails , name='game'),
+    path('buyGame/<int:gameId>', views.buyGame, name='buy-game'),     
+    path('download/<int:game_id>/', views.download_game_images, name='download_game'),
+    
+    path('category/<int:categoryId>/' , views.categoryGame , name='category-game'),
     path('addWishList/<int:gameId>', views.addWishList, name='add-wishlist'),     
     path('delWishList/<int:gameId>', views.delWishList, name='del-wishlist'),     
-    path('buyGame/<int:gameId>', views.buyGame, name='buy-game'),     
-    
-    path('download/<int:game_id>/', views.download_game_images, name='download_game'),
        
+    path('coins/', views.coins , name='coins'),
     path('chat/<int:game_id>/', views.chatRooms, name='chat-rooms'),
 
-
+    path('logout/', views.userLogout, name='user-logout'),
 ]
