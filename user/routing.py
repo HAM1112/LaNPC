9ef666a4ab23fea318a7536c18e8d662a9ba2ca2
Hamdan Salih <hamdansalihpc@gmail.com>
2023-09-17 11:41:52 +0530

    code sturcture

--- user/routing.py
+++ user/routing.py
@@ -1,8 +1,6 @@
-from django.urls import path
 from . import consumers
+from django.urls import path
 
 websocket_urlpatterns = [
-    path('user/chat/<int:game_id>/', consumers.ChatConsumer.as_asgi()),
-    
-    
+    path('user/chat/<int:game_id>/', consumers.ChatConsumer.as_asgi()), 
 ]
