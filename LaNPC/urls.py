fb95740fd9ab68a1498fa22dd4504adb0c534fe5
Hamdan Salih <hamdansalihpc@gmail.com>
2023-10-02 11:39:02 +0530

    static files

--- LaNPC/urls.py
+++ LaNPC/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', RedirectView.as_view(url='home/', permanent=False)),
@@ -26,4 +28,7 @@ urlpatterns = [
     path('user/' , include('user.urls')),
     # paypal url
     path('paypal/' , include('paypal.standard.ipn.urls')),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
